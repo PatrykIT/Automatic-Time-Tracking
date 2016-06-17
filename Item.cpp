b41506f25421d5a1bfd4060fc097e18c94959193
PatrykIT <the.last.of.us.08.08@gmail.com>
2016-06-17 14:33:25 +0200

    Wrapped DIR pointer with unique_ptr

--- Item.cpp
+++ Item.cpp
@@ -38,21 +38,20 @@ Item::Item(Item &&rhs) noexcept
 void Item::Load_Icon()
 {
     /* We will search for icon in 3 sizes: 32x32, 48x48, 64x64. First found icon size is loaded. */
-    std::vector<std::string> paths_to_icons = { icons_path + "32x32/apps/", icons_path + "48x48/apps/", icons_path + "64x64/apps/" };
-    DIR *directory;
+    std::vector<std::string> paths_to_icons = {icons_path, icons_path_2 + "32x32/apps/", icons_path_2 + "48x48/apps/", icons_path_2 + "64x64/apps/", icons_path_3 + "apps/32/", icons_path_3 + "apps/48/"};
     struct dirent *dir_struct;
 
     for(std::vector<std::string>::iterator path = paths_to_icons.begin(); path != paths_to_icons.end(); ++path)
     {
-        directory = opendir(path->c_str());
+        std::unique_ptr<DIR, decltype(&closedir)> directory (opendir(path->c_str()), closedir);
         if(directory == NULL)
             continue;
 
-        /* Search whole directory to find a file with the same name as observed process, with .png extension */
-        //TO DO: Look also for .jpg etc. Would be more flexible.
-        while((dir_struct = readdir(directory)) != NULL)
+        /* Search whole directory to find a file with the same name as observed process, with .png or .svg extension */
+        while((dir_struct = readdir(directory.get())) != NULL)
         {
-            if(strcmp(dir_struct->d_name, (name + ".png").c_str()) == 0)
+            if(strcmp(dir_struct->d_name, (name + ".png").c_str()) == 0 ||
+                    strcmp(dir_struct->d_name, (name + ".svg").c_str()) == 0)
             {
                 /* Load icon from file */
                 icon = std::make_unique<QPixmap>(QString::fromStdString(*path + dir_struct->d_name));
