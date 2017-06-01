4bca838f05b64741155d907ddc55ce86656d9547
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-06-01 11:30:38 +0200

    Interfaces declared.

--- Manager.h
+++ Manager.h
@@ -16,27 +16,109 @@ struct Logger;
 
 struct Process_Statistics
 {
+    Process_Statistics();
+    ~Process_Statistics() = default;
+    Process_Statistics(int hours, int minutes, int seconds);
 
+    Process_Statistics(const Process_Statistics &statistics);
+    Process_Statistics(Process_Statistics &&rhs) noexcept;
+    Process_Statistics& operator = (const Process_Statistics &rhs)  = delete;
+
+    void Stop_Counting_Time();
+    void Parse_Time();
+    int Parse_Minutes();
+    int Parse_Seconds();
+
+    bool operator < (const Process_Statistics &rhs) const noexcept
+    {
+        return total_hours < rhs.total_hours;
+    }
+
+    typedef std::chrono::high_resolution_clock _clock;
+    std::chrono::time_point<_clock> begin_time;
+    std::chrono::time_point<_clock> end_time;
+
+    int total_hours, total_minutes, total_seconds; //Time that application was ran since beggining of watching.
+    bool is_running;
+
+    //typedef std::chrono::duration<double, std::ratio<1>> seconds; //It's more precise, but we don't need that much precision.
+    typedef std::chrono::seconds seconds;
 };
 
 
-class Abstract_Manager : public QObject
+
+class Manager_Interface
+{
+protected:
+    virtual void Add_Item_to_Observe(const Item &item, Process_Statistics time_stats) = 0;
+    virtual void Add_Item_to_Observe(Item &&item, Process_Statistics &&time_stats) = 0;
+
+    virtual void Check_if_Applications_are_Running() = 0;
+    virtual void Add_New_Observed_Objects() = 0;
+    virtual std::vector<std::string> Get_Running_Applications() = 0;
+
+    virtual void Save_Statistics_to_File() = 0;
+    virtual void Load_Statistics_from_File() = 0;
+    virtual std::tuple<std::string, int, int, int> Parse_File_Statistics(const std::string &line) const = 0;
+
+    Manager_Interface() { }
+
+public:
+    virtual void Start() = 0;
+
+    virtual ~Manager_Interface() = 0;
+};
+
+
+class Abstract_Manager : public QObject, protected Manager_Interface
 {
     Q_OBJECT
 
 /* Methods */
 protected:
-    virtual void Save_Statistics_to_File() = 0;
-    virtual void Load_Statistics_from_File() = 0;
-    virtual void LOGS(const Logger &info) const = 0;
+    //virtual void LOGS(const Logger &info) const = 0;
+
+     /* TODO: Implement default behaviour */
+    virtual void Add_Item_to_Observe(const Item &item, Process_Statistics time_stats);
+    virtual void Add_Item_to_Observe(Item &&item, Process_Statistics &&time_stats);
+
+    virtual void Check_if_Applications_are_Running();
+    virtual void Add_New_Observed_Objects();
+
+    virtual void Save_Statistics_to_File();
+    virtual void Load_Statistics_from_File();
+    virtual std::tuple<std::string, int, int, int> Parse_File_Statistics(const std::string &line) const;
 
 /* Members */
 protected:
     Process_Statistics proccess_statistic_object;
     std::vector<std::pair<Item, Process_Statistics>> applications;
-    std::fstream file_stats;
-    const std::string path_to_stats_file;
+    std::vector<std::string> processes_names;
 
+    std::fstream file_stats;
+    std::string path_to_stats_file;
+
+    Abstract_Manager() { }
+
+
+public:
+    virtual void Print_Elapsed_Time() const;
+
+signals:
+    void Show_Icon(QPixmap icon);
+};
+
+class Linux_Manager : private Abstract_Manager
+{
+    Q_OBJECT
+
+private:
+    std::string System_Call(const std::string &command) const;
+    std::set<int> Get_PIDs_from_Strings(std::vector<std::string> &input) const;
+    std::vector<std::string> Split_Command_Output_to_Strings(const std::string &input) const;
+    std::vector<std::string> Get_Processes_Names(const std::set<int> &pid_numbers) const;
+    std::vector<std::string> Get_Running_Applications();
+public:
 
 };
 
@@ -52,6 +134,11 @@ private:
 public:
     void Log_To_File(std::string log_message); //TODO: Add implementation
 };
+
+
+
+
+
 
 class Manager : public QObject
 {
@@ -110,11 +197,15 @@ private:
     void Add_New_Observed_Objects(std::vector<std::string> &processes_names);
 
     std::tuple<std::string, int, int, int> Parse_File_Statistics(const std::string &line) const;
+
+    /* --- Linux only --- */
     std::string System_Call(const std::string &command) const;
     std::set<int> Get_PIDs_from_Strings(std::vector<std::string> &input) const;
     std::vector<std::string> Split_Command_Output_to_Strings(const std::string &input) const;
     std::vector<std::string> Get_Processes_Names(const std::set<int> &pid_numbers) const;
-    std::vector<std::string> Observe();
+    /* ----------------- */
+
+    std::vector<std::string> Get_Running_Applications();
 
 
 //Members
